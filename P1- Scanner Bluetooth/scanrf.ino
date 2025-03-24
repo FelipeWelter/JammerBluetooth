e43eda8d7a8a0e03ac00391fce64290676ac26f7
FelipeWelter <feliwelter45@gmail.com>
2025-03-24 13:58:28 -0300

    scanner código

---
+++ P1- Scanner Bluetooth/scanrf.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <RF24.h>
+
+#define CE_PIN   7
+#define CSN_PIN  8
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+const uint8_t canais_bluetooth[] = { 
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+    33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62,
+    63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80
+};
+
+const int num_canais = sizeof(canais_bluetooth) / sizeof(canais_bluetooth[0]);
+
+void setup() {
+    Serial.begin(9600);
+    SPI.begin();
+    radio.begin();
+    radio.setAutoAck(false);
+    radio.setPALevel(RF24_PA_HIGH);
+    Serial.println("Escaneando canais Bluetooth...");
+}
+
+void loop() {
+    for (int i = 0; i < num_canais; i++) {
+        radio.setChannel(canais_bluetooth[i]);
+        radio.startListening();
+        delay(5);
+
+        if (radio.testCarrier()) {  // Se detectar um sinal forte no canal
+            Serial.print("Sinal detectado no canal: ");
+            Serial.println(canais_bluetooth[i]);
+        }
+
+        radio.stopListening();
+    }
+    Serial.println("Escaneamento completo! Reiniciando...");
+    delay(1000);
+}
